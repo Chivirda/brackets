28c5ef6770406d8825b6d821c7bca91ed199d436
Sergey Chivirda <si.chivirda@gmail.com>
2021-03-14 20:27:24 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let pattern = [];
+  let controlStr = str;
+
+  bracketsConfig.forEach(element => pattern.push(`${element[0]}${element[1]}`));
+
+  while (true) {
+    for (let i of pattern)
+      str = str.replace(i, '');
+
+    if (str.length === 0)
+      return true;
+    if (str === controlStr)
+      return false;
+
+    controlStr = str;
+  }
 }
